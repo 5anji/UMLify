69bf6fdd1bf20762ffc0baaf2158871499296c78
Sevastian Bajenov <sevabaj@gmail.com>
2023-06-09 18:32:27 +0300

    Adjust parser validation added by 5anji.

--- uml/src/main/java/com/lexer/Lexer.java
+++ uml/src/main/java/com/lexer/Lexer.java
@@ -61,24 +61,29 @@ public class Lexer {
 
     private String[] tokenize(String l) {
         StringBuffer sb = new StringBuffer();
+        if (l.length() == 0) {
+            System.err.println("ERROR: no tokens found");
+            System.err.flush();
+            System.exit(1);
+        } else {
+            for (int i = 0; i < l.length() - 1; i++) {
+                char curr = l.charAt(i);
+                char next = l.charAt(i + 1);
 
-        for (int i = 0; i < l.length() - 1; i++) {
-            char curr = l.charAt(i);
-            char next = l.charAt(i + 1);
+                sb.append(curr);
 
-            sb.append(curr);
-
-            if ((Character.isLetter(curr) || Character.isDigit(curr)) && !Character.isLetter(next) && next != 39
-                    && next != ' ') {
-                sb.append(" ");
-            } else if (curr >= 60 && curr < 62) {
-                if (Character.isLetter(next) || Character.isDigit(next) || next == 39) {
+                if ((Character.isLetter(curr) || Character.isDigit(curr)) && !Character.isLetter(next) && next != 39
+                        && next != ' ') {
+                    sb.append(" ");
+                } else if (curr >= 60 && curr < 62) {
+                    if (Character.isLetter(next) || Character.isDigit(next) || next == 39) {
+                        sb.append(" ");
+                    }
+                } else if (curr == 39 && !Character.isLetter(next)) {
+                    sb.append(" ");
+                } else if (curr == 62) {
                     sb.append(" ");
                 }
-            } else if (curr == 39 && !Character.isLetter(next)) {
-                sb.append(" ");
-            } else if (curr == 62) {
-                sb.append(" ");
             }
         }
         sb.append(l.charAt(l.length() - 1));
--- uml/src/main/java/com/parser/Parser.java
+++ uml/src/main/java/com/parser/Parser.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 import com.lexer.Lexer;
 import com.lexer.Token;
@@ -15,48 +17,84 @@ public class Parser {
     private Lexer lexer;
     private List<Element> parseList;
 
-    public Parser () {
+    private boolean containsNonNumeric(String str) {
+        String temp = str.replace("'", "");
+        String pattern = "^(?![0-9']+$).*";
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(temp);
+        return matcher.matches();
+    }
+
+    public Parser() {
         lexer = new Lexer();
         parseList = new ArrayList<>();
     }
 
-    public List<Element> getParseList () {
-        return this.parseList;
+    public List<Element> getParseList() {
+        return parseList;
     }
 
-    public void fillParseList (String path) {
+    public void fillParseList(String path) {
         List<Token> tokens = lexer.analyze(path);
 
         int index = 0;
-        while (index < tokens.size() - 1) {
+
+        boolean tagStarted = false;
+        while (index < tokens.size()) {
             byte marker = 0;
             String elementName = tokens.get(index).getLiteral().toLowerCase();
+            boolean entityChecker = tokens.get(index).getCategory().equals(Category.ENTITY.toString());
+            boolean relationChecker = tokens.get(index).getCategory().equals(Category.RELATION.toString());
 
-            if (tokens.get(index).getCategory().equals(Category.ENTITY.toString()) 
-                ||  tokens.get(index).getCategory().equals(Category.RELATION.toString())) {
+            if (!containsNonNumeric(elementName)) {
+                System.err.println("WARNING: only digits are inside element  >>> " + elementName + " <<<");
+                System.err.flush();
+            }
+
+            if (tokens.get(index).getCategory().equals(Category.TAG_START.toString())) {
+                tagStarted = true;
+            }
+
+            if (tokens.get(index).getCategory().equals(Category.TAG_END.toString())) {
+                if (!tagStarted) {
+                    System.err.println("ERROR: cannot find tag beginning");
+                    System.err.flush();
+                    System.exit(1);
+                } else {
+                    tagStarted = false;
+                }
+            }
+
+            if (entityChecker || relationChecker) {
                 Element e = new Element(elementName);
                 Map<String, String> attributes = new HashMap<>();
                 index++;
-                
-                while ((!tokens.get(index).getCategory().equals(Category.ENTITY.toString()) 
-                        &&  !tokens.get(index).getCategory().equals(Category.RELATION.toString())) 
-                        &&  index < tokens.size() - 1) {
+
+                while (!entityChecker && !relationChecker && index < tokens.size()) {
                     if (tokens.get(index).getCategory().equals(Category.ATTRIBUTE.toString())) {
-                        attributes.put(tokens.get(index).getLiteral(), tokens.get(index + 2).getLiteral().replace("'", ""));
-                        index += 3;
-                    } else {
-                        index++;
+                        attributes.put(
+                                tokens.get(index).getLiteral(),
+                                tokens.get(index + 2).getLiteral().replace("'", ""));
+
+                        index += 2;
                     }
+                    index++;
                 }
 
                 e.setAttributes(attributes);
-                this.parseList.add(e);
+                parseList.add(e);
                 marker++;
             }
 
             if (marker == 0) {
                 index++;
             }
+
+        }
+        if (tagStarted) {
+            System.err.println("ERROR: cannot find tag ending");
+            System.err.flush();
+            System.exit(1);
         }
     }
 }
